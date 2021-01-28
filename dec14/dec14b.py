15b67c545f02b7bd2da2f41838a44cd7f142704e
joeyviolacode <joeyviolacode@gmail.com>
2021-01-28 14:24:05 -0500

    14b - Properly masking with wild cards to produce full masked list of memory addresses.

--- dec14/dec14b.py
+++ dec14/dec14b.py
@@ -1,16 +1,19 @@
 from dec14_input import directions
 
 def get_binary_list(count):
-    return [bin(num)[2:].zfill(4) for num in list(range(2 ** count))]
+    return [bin(num)[2:].zfill(count) for num in list(range(2 ** count))]
 
 # incomplete
 def apply_mask(address, mask, bin_str):
     new_bin_str = ""
     for i in range(len(mask)):
         if mask[i] == "X":
-            new_bin_str += mask[i]
+            new_bin_str += bin_str[0]
+            bin_str = bin_str[1:]
+        elif mask[i] == "1":
+            new_bin_str += "1"
         else: 
-            new_bin_str += bin_num[i]
+            new_bin_str += address[i]
     return new_bin_str
 
 def get_masked_list(address, mask):
@@ -18,9 +21,12 @@ def get_masked_list(address, mask):
     masked_list = []
     for bin_str in bin_list:
         masked_list.append(apply_mask(address, mask, bin_str))
-    return bin_list
+    return masked_list
 
 
 
-# print(get_masked_list("00000000000000000000000000000000X0XX", "00000000000000000000000000000000X0XX"))
-print("12, 3, 14".split(", "))
+print(get_masked_list("00101011", "110101XX"))
+# print("12, 3, 14".split(", "))
+# print(get_binary_list(1))
+
+# print(get_binary_list("110101XX".count("X")))
