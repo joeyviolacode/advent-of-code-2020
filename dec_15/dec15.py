7953e3a3b2a2b6f3346087a8c4af6876df3b644e
joeyviolacode <joeyviolacode@gmail.com>
2021-02-01 13:22:49 -0500

    Changing out for Audacity.

--- dec_15/dec15.py
+++ dec_15/dec15.py
@@ -1,3 +1,29 @@
-first_turns = [-1,0,12,6,13,20,1,17]
+first_turns = [0,12,6,13,20,1,17]
+# first_turns = [0, 3, 6]
 
-print(first_turns)
+def calculate_next(num, turn, counts):
+    if num in counts:
+        next_num = turn - counts[num]
+    else:
+        next_num = 0
+    counts[num] = turn
+    return next_num, counts
+
+def initialize_count(game):
+    counts = {}
+    for turn, num in enumerate(game):
+        counts[num] = turn + 1    
+    return counts
+
+def build_game():
+    pass
+
+def find_2020th(first_turns):
+    game = first_turns
+    next_turn = len(game)
+    count = initialize_count(game)
+    print(calculate_next(0, 8, count))
+
+
+
+print(find_2020th(first_turns))
