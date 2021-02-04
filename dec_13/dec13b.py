7b040da2421896f7af0f8106e4f4bdf77683d18a
joeyviolacode <joeyviolacode@gmail.com>
2021-02-03 22:31:07 -0500

    Removed some print statements for blog article on Problem 13

--- dec_13/dec13b.py
+++ dec_13/dec13b.py
@@ -4,7 +4,7 @@ from dec13_input import schedule_spaced as schedule
 # bus2 - bus1 will always equal the delay when bus1 is multiplied by n * interval for any 
 # positive whole number n
 def find_start(bus1, bus2, interval, delay):
-    print("Round: " + str(bus1) + " " + str(bus2) + ", interval: " + str(interval))
+    print("Round: " + str(bus1) + " and " + str(bus2) + ", interval: " + str(interval))
     for i in range(bus1, interval * bus2, interval):
         print("checking: " + str(i))
         if (i + delay) % bus2 == 0:
@@ -14,11 +14,9 @@ def find_start(bus1, bus2, interval, delay):
 def calculate_mods(schedule):
     bus1 = schedule[0]["bus"]
     interval = bus1
-    print(schedule)
     schedule = schedule[1:]
     while len(schedule) > 0: 
         bus1, interval = find_start(bus1, schedule[0]["bus"], interval, schedule[0]["delay"])
-        print("bus1: " + str(bus1))
         schedule = schedule[1:]
     return bus1
 
